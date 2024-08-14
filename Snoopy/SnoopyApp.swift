1fb541c9c8f675696b0e40d4504fa5bf351c40f2
Jared Nash <104384562+nashjar000@users.noreply.github.com>
2024-08-14 10:57:00 -0700

    Initial Commit

---
+++ Snoopy/SnoopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoopyApp.swift
+//  Snoopy
+//
+//  Created by Jared Nash on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SnoopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
