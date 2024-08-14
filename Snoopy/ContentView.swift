eaa7f1ab9ee5d2dd71f9ba24d4d5f3f6e60cca38
Jared Nash <104384562+nashjar000@users.noreply.github.com>
2024-08-14 11:14:59 -0700

    snoopy app

--- Snoopy/ContentView.swift
+++ Snoopy/ContentView.swift
@@ -6,19 +6,51 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    // AVPlayer for playing sound
+    var player: AVPlayer?
+    
+    init() {
+        // Load the sound file
+        if let soundURL = Bundle.main.url(forResource: "Doctor_Who_Theme_2024", withExtension: "mp3") {
+            player = AVPlayer(url: soundURL)
         }
-        .padding()
     }
-}
+    
+    var body: some View {
+        
+        ZStack {
+            Color(.black).edgesIgnoringSafeArea(.all)
+            VStack {
+                Button(action: {
+                    // Play the sound when image is tapped
+                    self.player?.play()
+                }) {
+                    Image("DoctorSnoopy")
+                        .resizable()
+                        .cornerRadius(50)
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                }
+                .buttonStyle(PlainButtonStyle()) // Remove default button style
+                
+                Text("Doctor Snoopy")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                
+                // Optional: Add a share link if needed
+                ShareLink(item: /*@START_MENU_TOKEN@*/URL(string: "https://developer.apple.com/xcode/swiftui")!/*@END_MENU_TOKEN@*/)
+                }
+            }
+        }
+    }
+
 
 #Preview {
     ContentView()
 }
+
